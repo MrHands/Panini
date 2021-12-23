7e73a59a8bd0a5ffb2764402e0e55c0e690bbd5e
Mr. Hands <mrhands31@gmail.com>
2021-12-23 12:56:15 +0100

    Document Config struct.

--- include/Config.hpp
+++ include/Config.hpp
@@ -6,24 +6,43 @@
 #include <stdint.h>
 #include <string>
 
+/*!
+	\file
+	\defgroup Enums Global enumeration values.
+*/
+
 namespace panini
 {
-
+	/*!
+		\brief Brace breaking style to use when writing to output.
+		\ingroup Enums
+	*/
 	enum class BraceBreakingStyle
 	{
-		Inherit, // Inherit setting from the config
-		Attach, // Open brace on the same line
-		Allman, // New line before brace open
-		Whitesmiths // New line and indent before brace open and brace close
+		Inherit,      //!< Inherit setting from the config, not valid on @ref WriterBase
+		Attach,       //!< Open brace on the same line
+		Allman,       //!< New line before brace open
+		Whitesmiths   //!< New line and indent before brace open and brace close
 	};
 
+	/*!
+		\brief Include style to use when writing to output.
+		\ingroup Enums
+	*/
 	enum class IncludeStyle
 	{
-		DoubleQuotes,
-		SingleQuotes,
-		AngularBrackets
+		DoubleQuotes,    //!< Output double quotation marks ""
+		SingleQuotes,    //!< Output single quotation marks '' (not valid in C++)
+		AngularBrackets  //!< Output angular brackets <>
 	};
 
+	/*!
+		\brief Global configuration applied to writers.
+
+		Configuration is applied to all commands when they are processed by
+		@ref WriterBase. Some commands can override these configuration
+		settings.
+	*/
 	struct Config
 	{
 		BraceBreakingStyle braces = BraceBreakingStyle::Allman;

06b0f7bad211d48756a73cd8e65e6f0ce4ecc264
Mr. Hands <mrhands31@gmail.com>
2023-06-28 15:19:30 +0200

    Update documentation.

--- documentation/classpanini_1_1_feature_flag.js
+++ documentation/classpanini_1_1_feature_flag.js
@@ -1,7 +1,7 @@
 var classpanini_1_1_feature_flag =
 [
-    [ "TCallback", "classpanini_1_1_feature_flag.html#ad0c5cab42f4f0257360cbed95ce427f1", null ],
+    [ "TCallback", "classpanini_1_1_feature_flag.html#a7a0e7dc458cd188601897e360c4b4c8c", null ],
     [ "FeatureFlag", "classpanini_1_1_feature_flag.html#ac652108c76450befd8a00bf548f07cd2", null ],
     [ "FeatureFlag", "classpanini_1_1_feature_flag.html#a4050e003abdd312b1b8a6b0c1c662454", null ],
-    [ "Visit", "classpanini_1_1_feature_flag.html#a43a3ccb9c2cf2895110b5b48368dd34e", null ]
+    [ "Visit", "classpanini_1_1_feature_flag.html#a24712739c1ddc410dcd24b9d8fcb6a5a", null ]
 ];
--- documentation/search/all_10.js
+++ documentation/search/all_10.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['targetpath_0',['targetPath',['../structpanini_1_1_file_writer_config.html#afbec153091d86d7f2ba28e3d4c55951a',1,'panini::FileWriterConfig']]],
-  ['tcallback_1',['TCallback',['../classpanini_1_1_braces.html#a849aa2e5e8d80fbf45a9649d3b50296c',1,'panini::Braces::TCallback()'],['../classpanini_1_1_feature_flag.html#ad0c5cab42f4f0257360cbed95ce427f1',1,'panini::FeatureFlag::TCallback()'],['../classpanini_1_1_scope.html#a7ce1ad35e795c9c1ec1dba8d0ebe5d4f',1,'panini::Scope::TCallback()']]],
+  ['tcallback_1',['TCallback',['../classpanini_1_1_braces.html#a849aa2e5e8d80fbf45a9649d3b50296c',1,'panini::Braces::TCallback()'],['../classpanini_1_1_feature_flag.html#a7a0e7dc458cd188601897e360c4b4c8c',1,'panini::FeatureFlag::TCallback()'],['../classpanini_1_1_scope.html#a7ce1ad35e795c9c1ec1dba8d0ebe5d4f',1,'panini::Scope::TCallback()']]],
   ['ttransform_2',['TTransform',['../classpanini_1_1_comma_list.html#a27f21f2ed6ef063de4ef8db7f34ff451',1,'panini::CommaList']]],
   ['tunderlying_3',['TUnderlying',['../classpanini_1_1_comma_list.html#a3ed1960e25cd1de99b8d83e702f4af53',1,'panini::CommaList']]]
 ];
